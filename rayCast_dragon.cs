354ce76c7aa8d0cdebb82f9a43f542da4927b17d
jjypainter <jypainter@naver.com>
2020-01-16 23:38:20 +0900

    레이캐스트 드래곤 파일 스크립트 작성(로봇과 동일). 비슷하게 초록색 광선이 안나옴

--- rayCast_dragon.cs
+++ rayCast_dragon.cs
@@ -13,6 +13,16 @@ public class rayCast_dragon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        RaycastHit hit;
+        //부딛히는 물체의 정보를 담는 변수 hit선언
+        Vector3 forward = transform.TransformDirection(Vector3.forward)*1000;
+        //forward라는 이름의 방향변수 선언및 대입
+        Debug.DrawRay(transform.position, forward, Color.green);
+        //가상의 레이인 레이캐스트를 테스트 중에는 보일 수 있도록 함
+        if (Physics.Raycast(transform.position, forward, out hit)){
+            Debug.Log("hit");
+            //만약에 레이캐스트가 어떠한 물체에 맞는다면 hit라는 문구를 냄
+        }
         
     }
 }
