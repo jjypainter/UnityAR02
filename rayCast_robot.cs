62eaeb04a32588be40eff7a49e03861f26788b99
jjypainter <jypainter@naver.com>
2020-01-17 23:32:43 +0900

    스크립트 바꿈. 뭐가 잘못된걸까

--- rayCast_robot.cs
+++ rayCast_robot.cs
@@ -22,25 +22,26 @@ public class rayCast_robot : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    { RaycastHit hit;
+    { 
+        RaycastHit hit;
     //부딛치는 물체의 정보를 담는 변수 hit 선언
-    Vector3 forward = transform.TransformDirection(Vector3.forward)*1000;
+      Vector3 forward = transform.TransformDirection(Vector3.forward)*1000;
     //forward라는 이름의 방향변수 선언 및 대입
-    Debug.DrawRay(transform.position, forward, Color.green);
+      Debug.DrawRay(transform.position, forward, Color.green);
     //가상의 레이인 레이캐스트를 테스트 중에는 보일 수 있도록 함
-    if (Physics.Raycast (transform.position, forward, out hit)){
-        Debug.Log("hit");
-        anim.SetBool("isHit",true);
+      if (Physics.Raycast (transform.position, forward, out hit)){
+                  Debug.Log("hit");
+                  anim.SetBool("isHit",true);
 
-        timeElapsed=timeElapsed+Time.deltaTime; //초 단위로 지나가는 시간
-        if (timeElapsed >=3){ //3초에 한번씩 공격
-            hit.transform.GetComponent<rayCast_dragon> ().HltPnt=
-                hit.transform.GetComponent<rayCast_dragon>().HltPnt-AtkPnt; //상대방의 체력에서 공격력을 뺀다.
-            timeElapsed=0; //시간 초기화
-        }
-    } else{
-         anim.SetBool("isHit", false);
-    }
+                  timeElapsed=timeElapsed+Time.deltaTime; //초 단위로 지나가는 시간
+                  if (timeElapsed >=3){ //3초에 한번씩 공격
+                             hit.transform.GetComponent<rayCast_dragon> ().HltPnt=
+                                        hit.transform.GetComponent<rayCast_dragon>().HltPnt-AtkPnt; //상대방의 체력에서 공격력을 뺀다.
+                             timeElapsed=0; //시간 초기화
+                 }
+     } else{
+                 anim.SetBool("isHit", false);
+     }
         
-    }
+     }
 }
