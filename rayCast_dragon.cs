2e3db82785f0b2f23824c7d9c68cc728532bf6d4
jjypainter <jypainter@naver.com>
2020-01-19 23:44:32 +0900

    스크립트 바꿈., 현재 문제 1. 로봇은 공격포즈가 나오지 않고, 용을 공격하지 않는다. 2. 용은 애니메이션이 바로 쓰러진다. 그리고 로봇을 공격하지 않는다.

--- rayCast_dragon.cs
+++ rayCast_dragon.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 
 public class rayCast_dragon : MonoBehaviour
 {
+    public bool isDetected;
     Animator anim;
 
-    public int AtkPnt;
-    public int HltPnt;
+    public int atkPnt;
+    public int hltPnt;
 
     float timeElapsed;
+    public GameObject enemy;
     // Start is called before the first frame update
     void Start()
     {
         anim=transform.GetComponent<Animator> ();
 
-        AtkPnt=200; //공격력
-        HltPnt=5000; //체력
+        atkPnt=200; //공격력
+        hltPnt=5000; //체력
         
     }
 
@@ -25,24 +27,27 @@ public class rayCast_dragon : MonoBehaviour
     {
         RaycastHit hit;
         //부딛히는 물체의 정보를 담는 변수 hit선언
-        Vector3 forward = transform.TransformDirection(Vector3.forward)*1000;
+        Vector3 forwardDir = transform.TransformDirection(Vector3.forward)*1000;
         //forward라는 이름의 방향변수 선언및 대입
-        Debug.DrawRay(transform.position, forward, Color.green);
+        Debug.DrawRay(this.transform.position, forwardDir, Color.green);
+
+        isDetected = Physics.Raycast (this.transform.position, forwardDir, out hit);
         //가상의 레이인 레이캐스트를 테스트 중에는 보일 수 있도록 함
-        if (Physics.Raycast(transform.position, forward, out hit)){
+        if (isDetected){
             Debug.Log("hit");
             anim.SetBool("isHit", true);
+
             timeElapsed=timeElapsed+Time.deltaTime; //초 단위로 지나가는 시간
             if (timeElapsed>=3){ //3초에 한번씩 공격
-                hit.transform.GetComponent<rayCast_robot>().HltPnt=
-                    hit.transform.GetComponent<rayCast_robot>().HltPnt-AtkPnt;
+                hit.transform.GetComponent<rayCast_robot>().hltPnt=
+                    hit.transform.GetComponent<rayCast_robot>().hltPnt-atkPnt;
                 //상대방의 체력에서 공격력을 뺀다
                 timeElapsed=0; //시간 초기화
             }
         }else{
             anim.SetBool("isHit", false);
         }
-        if(HltPnt<=0){
+        if(hltPnt<=0){
             anim.SetBool("isDead", true);
         }
         
